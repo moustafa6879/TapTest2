20d8f4e56feac649999f71c942bf207a0a755cfa
Mostafa Ghareeb <mghareeb@sumerge.com>
2023-10-22 16:49:41 +0300

    stuffed main loop in initialize()

--- src/ShareTrader.java
+++ src/ShareTrader.java
@@ -2,26 +2,7 @@ import java.util.Scanner;
 
 public class ShareTrader {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
-        int c1 = scanner.nextInt();
-        int c2 = scanner.nextInt();
-
-        if (validateInput(n, c1, c2)) {
-            ShareBuy buyTransaction = new ShareBuy(c1);
-            ShareSell sellTransaction = new ShareSell(c2);
-
-            for (int i = 0; i < n; i++) {
-                int price = scanner.nextInt();
-                processTransaction(buyTransaction, sellTransaction, price, i, n);
-            }
-
-            int finalBalance = buyTransaction.getBalance() + sellTransaction.getBalance();
-            System.out.println(finalBalance);
-        } else {
-            System.out.println("Invalid input.");
-        }
+            initialize();
     }
 
     private static boolean validateInput(int n, int c1, int c2) {
@@ -32,7 +13,28 @@ public class ShareTrader {
         return price >= 1 && price <= 1000;
     }
 
-    private static void processTransaction(ShareBuy buyTransaction, ShareSell sellTransaction, int price, int currentIndex, int n) {
+    private static void initialize(){
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int c1 = scanner.nextInt();
+        int c2 = scanner.nextInt();
+        int finalBalance = 0;
+
+        if (validateInput(n, c1, c2)) {
+            ShareBuy buyTransaction = new ShareBuy(c1);
+            ShareSell sellTransaction = new ShareSell(c2);
+
+            for (int i = 0; i < n; i++) {
+                int price = scanner.nextInt();
+                finalBalance =  processTransaction(buyTransaction, sellTransaction, price, i, n);
+            }
+            System.out.println(finalBalance);
+        }
+
+    }
+    private static int processTransaction(ShareBuy buyTransaction, ShareSell sellTransaction, int price, int currentIndex, int n) {
+        int balanceReturn = 0;
         if (validatePrice(price)) {
             if (currentIndex == n - 1) {
                 sellTransaction.setLast(true);
@@ -41,10 +43,14 @@ public class ShareTrader {
                 buyTransaction.process(price);
                 sellTransaction.process(price);
             }
+            balanceReturn = buyTransaction.getBalance() + sellTransaction.getBalance();
+
+
         } else {
             System.out.println("Invalid input.");
             System.exit(1);
         }
+        return balanceReturn;
     }
 
 }
